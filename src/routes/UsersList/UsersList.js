8e9b771aabdb29c1c204845771da46628108d8f9
umar87 <umar.ashfaq@emumba.com>
2017-10-08 14:15:05 +0500

    Adds react-toolbox

--- src/routes/UsersList/UsersList.js
+++ src/routes/UsersList/UsersList.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
+// src
+import './UsersList.css'
+
 export default class UsersList extends React.Component {
     render() {
-        return <div>User List</div>
+        return (
+            <div>
+                <header></header>
+                <main role="main">
+                    <article>
+                        
+                    </article>
+                </main>
+                <footer></footer>
+            </div>
+        )
     }
 }

ad97d550dcb295017d01c0464b73bf50ae507128
umar87 <umar.ashfaq@emumba.com>
2017-10-11 15:22:51 +0500

    Creates homepage

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+import Button from 'react-toolbox/lib/button/Button'
 
-export default class Home extends React.Component {
-    render() {
-        return <h1>Hello</h1>
+export default connect()(class Home extends React.Component {
+    handleClickGroups = () => {
+        const { dispatch } = this.props
+        dispatch(push('/groups'))
     }
-}
+    handleClickUsers = () => {
+        const { dispatch } = this.props
+        dispatch(push('/users'))
+    }
+    render() {
+        return (
+            <article className="Home">
+                <h1 className="Home-Heading">MyMeetups</h1>
+                <p className="Home-Tagline">An app that demonstrates use of React, Redux, Firebase, React-Toolbox, and ES6</p>
+                <div className="Home-Actions">
+                    <Button label="View Users" accent raised onClick={this.handleClickUsers}/> <Button label="View Groups" accent raised onClick={this.handleClickGroups}/>
+                </div>
+            </article>
+        )
+    }
+})
